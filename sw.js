1472d38867924bb83ffd20262c0c08951c8ce6f1
Maniyaro <newbwrk3@gmail.com>
2026-08-15 23:57:15 +0530

    Implement dynamic Unicode transliterator to translate all 37 taals on-the-fly

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'harmony-music-app-v105';
+const CACHE_NAME = 'harmony-music-app-v106';
 const ASSETS = [
   './', './index.html', './login.html', './dashboard.html', './profile.html', './daily-riyaz.html',
   './pitch-monitor.html', './pitch-monitor.css',
