cbfcbcc65aff9f4a0e46d1a995577100c99bf89a
Maniyaro <newbwrk3@gmail.com>
2026-08-15 16:07:07 +0530

    chore: bump cache version to v85

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'harmony-music-app-v84';
+const CACHE_NAME = 'harmony-music-app-v85';
 const ASSETS = [
   './', './index.html', './login.html', './dashboard.html', './profile.html', './daily-riyaz.html',
   './pitch-monitor.html', './pitch-monitor.css',
