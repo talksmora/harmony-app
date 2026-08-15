a1bae9396fcb2c9a3056819940d3380a00bd2640
Maniyaro <newbwrk3@gmail.com>
2026-08-15 22:34:47 +0530

    Fix Web Audio API past-time scheduling bug in virtual tabla sound generator

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'harmony-music-app-v94';
+const CACHE_NAME = 'harmony-music-app-v95';
 const ASSETS = [
   './', './index.html', './login.html', './dashboard.html', './profile.html', './daily-riyaz.html',
   './pitch-monitor.html', './pitch-monitor.css',
