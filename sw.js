6c59019c15c16a6a1d210ea7621984884022afed
Maniyaro <newbwrk3@gmail.com>
2026-08-15 22:27:58 +0530

    Update Metronome card with a custom SVG metronome icon

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'harmony-music-app-v92';
+const CACHE_NAME = 'harmony-music-app-v93';
 const ASSETS = [
   './', './index.html', './login.html', './dashboard.html', './profile.html', './daily-riyaz.html',
   './pitch-monitor.html', './pitch-monitor.css',
