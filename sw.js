68d7d63dc7c92c1fcf73c37c2629273d37bc724e
Maniyaro <newbwrk3@gmail.com>
2026-08-15 23:02:42 +0530

    Add full 37 Taals with compound bol splitting and premium vertical beat display from 12Notez

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'harmony-music-app-v98';
+const CACHE_NAME = 'harmony-music-app-v99';
 const ASSETS = [
   './', './index.html', './login.html', './dashboard.html', './profile.html', './daily-riyaz.html',
   './pitch-monitor.html', './pitch-monitor.css',
